b8756a99187c07ec30932c39aa61591d99fa3e38
idkanythingbro <rajsah18022@gmail.com>
2024-12-15 21:30:06 +0530

    Working on edit

--- learnhubclient/src/components/uicomponents/DashBoard.jsx
+++ learnhubclient/src/components/uicomponents/DashBoard.jsx
@@ -8,12 +8,39 @@ import {
   userLocation,
   orgName,
 } from "../../assets/constant";
+
+import { Popover } from "flowbite-react";
+
 import copy from "./../../assets/copy.svg";
+import edit from "./../../assets/edit.svg";
 const DashBoard = () => {
+  const goalsList = [
+    { id: 1, goal: "Learn ReactJs" },
+    { id: 2, goal: "Learn Flutter" },
+    { id: 3, goal: "Earn 1000000" },
+    { id: 4, goal: "Complete Project" },
+    { id: 5, goal: "Buy a Bike" },
+  ];
+
+  const goals = goalsList.map((item) => {
+    return (
+      <li key={item.id} className="border-2 rounded">
+        <p className="text-base font-semibold">{item.goal}</p>{" "}
+        <div className="flex justify-center items-center gap-3">
+          <input type="checkbox" className="" />
+          <p>Mark as Done</p>
+        </div>
+      </li>
+    );
+  });
+
   return (
     <div className="flex flex-col h-full w-full p-2">
-      <div className="flex lg:flex-row flex-col h-[50%] lg:h-1/3 p-2 shadow-lg gap-2 dark:bg-slate-800 overflow-scroll scrollbar-hidden">
+      <div className="flex lg:flex-row flex-col p-2 shadow-lg gap-2 dark:bg-slate-800 overflow-scroll scrollbar-hidden">
         <div className="flex justify-center items-center flex-col rounded-md shadow-lg p-1 dark:bg-slate-800">
+          <div className="w-full flex justify-end">
+            <img src={edit} alt="Edit" />
+          </div>
           <img
             className="bg-black rounded-full w-[150px] md:w-[200px] lg:w-[225px] h-[150px] md:h-[200px] lg:h-[225px] m-auto object-contain border-1 border-solid"
             src="/src/assets/luffy.jfif"
@@ -29,14 +56,22 @@ const DashBoard = () => {
           </div>
         </div>
 
-        <div className="lg:w-[20%] w-full rounded-md p-4 justify-center dark:bg-slate-800">
+        <div className="relative lg:w-[20%] w-full rounded-md p-4 justify-center dark:bg-slate-800">
           <q className="text-md font-semibold dark:text-white italic">
             {userdescription}
           </q>
+          <button className="absolute right-0 top-0">
+            <img src={edit} alt="Edit" />
+          </button>
         </div>
         <div className="flex flex-col flex-grow md:w-[50%] w-full gap-4 pl-2 justify-center dark:text-white">
-          <span className="font-semibold text-lg shadow-sm w-full">
-            Name : {userName}
+          <span className="relative font-semibold text-lg shadow-sm w-full">
+            <div className="flex items-center">
+              Name : {userName}{" "}
+              <button className="absolute right-0">
+                <img src={edit} alt="Edit" className="" />
+              </button>
+            </div>
           </span>
           <span className="font-semibold text-lg shadow-sm w-full">
             Organization Name : {orgName}
@@ -55,12 +90,47 @@ const DashBoard = () => {
           </span>
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row h-[40%] gap-4 m-2">
-        <div className="w-full lg:w-[20%] bg-black rounded-2xl">Long Term Goals</div>
-        <div className="w-full lg:w-[20%] bg-black rounded-2xl">Yearly Goals</div>
-        <div className="w-full lg:w-[20%] bg-black rounded-2xl">Monthly Goals</div>
-        <div className="w-full lg:w-[20%] bg-black rounded-2xl">Weekly Goals</div>
-        <div className="w-full lg:w-[20%] bg-black rounded-2xl">Todays Goals</div>
+      <div className="flex flex-col lg:flex-row h-[40%] gap-4 mt-1 mb-1 ">
+        <div className="w-full lg:w-[20%] border-2 rounded-2xl dark:bg-slate-800">
+          <div className="text-2xl font-bold flex justify-center p-2 text-[#ffad33] dark:text-white">
+            Long Term Goals
+          </div>
+          <ul className="flex justify-center flex-col dark:text-white p-2 gap-2">
+            {goals}
+          </ul>
+        </div>
+        <div className="w-full lg:w-[20%] border-2 rounded-2xl dark:bg-slate-800">
+          <div className="text-2xl font-bold flex justify-center p-2 text-[#ffad33] dark:text-white">
+            Yearly Goals
+          </div>
+          <ul className="flex justify-center flex-col dark:text-white p-2 gap-2">
+            {goals}
+          </ul>
+        </div>
+        <div className="w-full lg:w-[20%] border-2 rounded-2xl dark:bg-slate-800">
+          <div className="text-2xl font-bold flex justify-center p-2 text-[#ffad33] dark:text-white">
+            Monthly Goals
+          </div>
+          <ul className="flex justify-center flex-col dark:text-white p-2 gap-2">
+            {goals}
+          </ul>
+        </div>
+        <div className="w-full lg:w-[20%] border-2 rounded-2xl dark:bg-slate-800">
+          <div className="text-2xl font-bold flex justify-center p-2 text-[#ffad33] dark:text-white">
+            Weekly Goals
+          </div>
+          <ul className="flex justify-center flex-col dark:text-white p-2 gap-2">
+            {goals}
+          </ul>
+        </div>
+        <div className="w-full lg:w-[20%] border-2 rounded-2xl dark:bg-slate-800">
+          <div className="text-2xl font-bold flex justify-center p-2 text-[#ffad33] dark:text-white">
+            Daily Goals
+          </div>
+          <ul className="flex justify-center flex-col dark:text-white p-2 gap-2">
+            {goals}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- learnhubclient/src/components/uicomponents/VideoPlayer.jsx
+++ learnhubclient/src/components/uicomponents/VideoPlayer.jsx
@@ -1,13 +1,8 @@
-import ReactPlayer from "react-player";
+
 const VideoPlayer = () => {
   return (
     <div className="flex w-full dark:bg-slate-800">
-      <ReactPlayer
-        url="https://youtu.be/4nzl8vDI43s"
-        controls={true}
-        width={"1280px"}
-        height={"720px"}
-      ></ReactPlayer>
+      
     </div>
   );
 };
